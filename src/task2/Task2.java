e98cc83f9c8141342ec0a914eb60bd5d6b5e8a31
MisoFana <rogiv.bogdan@gmail.com>
2024-09-04 19:24:21 +0300

    completed-task2-task3

---
+++ src/task2/Task2.java
@@ -0,0 +1,10 @@
+package task2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        String name = "Петро Іванович";
+        float salary = 10252.75f;
+        String result = String.format("Name: %s, Salary: %.2f", name, salary);
+        System.out.println(result);
+    }
+}
--- src/task3/Task3.java
+++ src/task3/Task3.java
@@ -1,33 +1,68 @@
 package task3;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try {
+            //Основа
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Довжина масиву: ");
-        int arrLength = scanner.nextInt();
+            System.out.print("Довжина масиву: ");
+            int arrLength = scanner.nextInt();
 
-        int[] arr = new int[arrLength];
-        for (int i = 0; i < arrLength; i++) {
-            System.out.print("Під індексом " + i + " буде: ");
-            arr[i] = scanner.nextInt();
+            int[] arr = new int[arrLength];
+            for (int i = 0; i < arrLength; i++) {
+                System.out.print("Під індексом " + i + " буде: ");
+                arr[i] = scanner.nextInt();
+            }
+
+            //Перша частина
+            int[] reversedArr = reverse(arr);
+            System.out.println("Реверсований масив:");
+            for (int j : reversedArr) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+
+            //Друга частина
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int j : arr) {
+                list.add(j);
+            }
+            System.out.println("Це новостворений список: " + list);
+
+            System.out.println("Добавляю число 999 вкінець списку");
+            list.add(999);
+            System.out.println("Ось,аби видно зміни було: " + list);
+
+            System.out.println("Видаляю, щойно доданий елемент за його індексом");
+            list.remove(Integer.valueOf(999));
+            System.out.println("Ось,аби видно зміни було: " + list);
+
+            System.out.println("Заміняю значення першого елементу");
+            if (!list.isEmpty()) {
+                list.set(0, 888);
+                System.out.println("Ось,аби видно зміни було: " + list);
+            }
+
+            System.out.println("Сортую...");
+            Collections.sort(list);
+            System.out.println("Ось,аби видно зміни було: " + list);
+
+            ArrayList<Integer> noneList = null;
+            noneList.add(1);
+        } catch (NullPointerException e) {
+            System.out.println("Error " + e.getMessage());
         }
-
-        int[] reversedArr = reverse(arr);
-        System.out.println("Реверсований масив:");
-        for (int num : reversedArr) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-
     }
 
     public static int[] reverse(int[] arr) {
         int[] payload = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
-            payload[i] =  arr[arr.length - 1 - i];
+            payload[i] = arr[arr.length - 1 - i];
         }
         return payload;
 
